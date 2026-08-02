255091a8696ca6e0f6154f3cf06dcfe1adf8335f
Muhammad Al-Muhammad <mm38454946@gmail.com>
2026-08-02 23:13:13 +0300

    Update server.js

--- my cafe/server.js
+++ my cafe/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const PORT = 3000;
 
 app.use(express.json());
